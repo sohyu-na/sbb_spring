f4de21981a312d88505d6538bc62174f57144d6f
sohyu-na <solim73@naver.com>
2024-12-18 00:02:29 +0900

    feat:답변 수정 기능 구현

--- sbb/src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ sbb/src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -1,6 +1,8 @@
 package com.mysite.sbb.answer;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
+import com.mysite.sbb.DataNotFoundException;
 
 import org.springframework.stereotype.Service;
 
@@ -22,6 +24,19 @@ public class AnswerService {
 		answer.setAuthor(author);
         this.answerRepository.save(answer);
 	}
-	
+	public Answer getAnswer(Integer id) {
+		Optional<Answer> answer = this.answerRepository.findById(id);
+		if(answer.isPresent()) {
+			return answer.get();
+		}
+		else {
+			throw new DataNotFoundException("answer not found");
+		}
+	}
 
+	public void modify(Answer answer, String content) {
+		answer.setContent(content);
+		answer.setModifyDate(LocalDateTime.now());
+		this.answerRepository.save(answer);
+	}
 }
